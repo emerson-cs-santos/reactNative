78e4ab0b7765d15aadade024f97a5d263adaf5e7
Emerson Costa <emersoncs@outlook.com.br>
2022-12-19 13:26:33 -0300

    Tela 4 finalizado

--- src/screens/Tela4/index.tsx
+++ src/screens/Tela4/index.tsx
@@ -5,6 +5,51 @@ import { Button } from './../../components/Button/index';
 import { useNavigation } from '@react-navigation/native';
 import { Input } from '../../components/Input';
 
+import RadioGroup, {RadioButtonProps} from 'react-native-radio-buttons-group';
+
+const radioButtonsData: RadioButtonProps[] = [
+    {
+        id: '1', // acts as primary key, should be unique and non-empty string
+        label: 'Feminino',
+        value: 'F',
+        selected: true
+    },
+
+    {
+        id: '2',
+        label: 'Masculino',
+        value: 'M'
+    },
+
+    {
+        id: '3',
+        label: 'Prefiro não informar',
+        value: 'O'
+    }
+]
+
+const radioButtonsData2: RadioButtonProps[] = [
+    {
+        id: '1', // acts as primary key, should be unique and non-empty string
+        label: 'Solteiro(a)',
+        value: 'S',
+        selected: true
+    },
+
+    {
+        id: '2',
+        label: 'Casado(a)',
+        value: 'C'
+    },
+
+    {
+        id: '3',
+        label: 'Viúvo(a)',
+        value: 'V'
+    }
+]
+
+
 export function Tela4() {
     const navigation = useNavigation();
 
@@ -15,14 +60,69 @@ export function Tela4() {
     const [nome, setNome] = useState('');
     const [sobreNome, setSobreNome] = useState('');
 
+    const [radioButtonGenero, setRadioButtons] = useState<RadioButtonProps[]>(radioButtonsData);
+
+    const [radioButtonsEstadoCivil, setRadioButtons2] = useState<RadioButtonProps[]>(radioButtonsData2);
+
     function msg() {
         if ( !nome || !sobreNome )
         {
             Alert.alert('Campos obrigatórios!', 'Preencha todos os campos!');
             return false;
         }
-        const mensagem: string = `Hello ${nome} ${sobreNome}`;
+
+        let genero: string = '';
+        radioButtonGenero.forEach(element =>
+        {
+            if ( element.selected )
+            {
+                genero = element.label;
+            }
+        });
+        // if ( radioButtonGenero[0].selected )
+        // {
+        //     genero = radioButtonGenero[0].label;
+        // }
+        // if ( radioButtonGenero[1].selected )
+        // {
+        //     genero = radioButtonGenero[1].label;
+        // }
+        // if ( radioButtonGenero[2].selected )
+        // {
+        //     genero = radioButtonGenero[2].label;
+        // }
+
+        let estadoCivil: string = '';
+        radioButtonsEstadoCivil.forEach(element =>
+        {
+            if ( element.selected )
+            {
+                estadoCivil = element.label;
+            }
+        });
+        // if ( radioButtonsEstadoCivil[0].selected )
+        // {
+        //     estadoCivil = radioButtonsEstadoCivil[0].label;
+        // }
+        // if ( radioButtonsEstadoCivil[1].selected )
+        // {
+        //     estadoCivil = radioButtonsEstadoCivil[1].label;
+        // }
+        // if ( radioButtonsEstadoCivil[2].selected )
+        // {
+        //     estadoCivil = radioButtonsEstadoCivil[2].label;
+        // }
+
+        const mensagem: string = `Hello ${nome} ${sobreNome} \nGênero: ${genero} \nEstado: ${estadoCivil}`;
         Alert.alert('Olá', mensagem);
+    }
+
+    function onPressRadioButton(radioButtonsArray: RadioButtonProps[]) {
+        setRadioButtons(radioButtonsArray);
+    }
+
+    function onPressRadioButton2(radioButtonsArray: RadioButtonProps[]) {
+        setRadioButtons2(radioButtonsArray);
     }
 
     return (
@@ -32,9 +132,10 @@ export function Tela4() {
             <Input onChangeText={setSobreNome} keyboardType="default" placeholder='SobreNome'/>
 
             <Text>Gênero</Text>
+            <RadioGroup radioButtons={radioButtonGenero} onPress={onPressRadioButton} />
 
-            <Text>Estado civil</Text>
-
+            <Text>Estado</Text>
+            <RadioGroup radioButtons={radioButtonsEstadoCivil} onPress={onPressRadioButton2}/>
 
             <Button title='Ver Informações' onPress={msg}/>
             <Button title='Home' onPress={rotaHome}/>
