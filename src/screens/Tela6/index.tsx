a0a803b9d9037a61815e5f2fc8fa0b457d0c36b2
Emerson Costa <emersoncs@outlook.com.br>
2022-12-19 20:05:16 -0300

    Continuando tela 6

--- src/screens/Tela6/index.tsx
+++ src/screens/Tela6/index.tsx
@@ -5,6 +5,8 @@ import { Button } from './../../components/Button/index';
 import { useNavigation } from '@react-navigation/native';
 import { Input } from '../../components/Input';
 
+import { SelectList } from 'react-native-dropdown-select-list'
+
 export function Tela6() {
     const navigation = useNavigation();
 
@@ -13,6 +15,18 @@ export function Tela6() {
     }
 
     const [nome, setNome] = useState('');
+
+    const [selected, setSelected] = useState('');
+
+    const data = [
+        {key:'1', value:'Mobiles', disabled:true},
+        {key:'2', value:'Appliances'},
+        {key:'3', value:'Cameras'},
+        {key:'4', value:'Computers', disabled:true},
+        {key:'5', value:'Vegetables'},
+        {key:'6', value:'Diary Products'},
+        {key:'7', value:'Drinks'},
+    ]
 
     function msg() {
         if ( !nome )
@@ -28,6 +42,12 @@ export function Tela6() {
         <Container>
             <Text>Tela 6</Text>
             <Input onChangeText={setNome} keyboardType="default" placeholder='Nome'/>
+
+            <SelectList
+            setSelected={(val) => setSelected(val)}
+            data={data}
+            save="value"
+            />
 
             <Button title='Home' onPress={rotaHome}/>
         </Container>
