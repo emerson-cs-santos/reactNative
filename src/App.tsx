d57350854715d6e49deeae288fa78d046c56b127
Emerson Costa <emersoncs@outlook.com.br>
2022-12-20 17:05:25 -0300

    Tela 6

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,35 @@ import { StyleSheet, Text, View } from 'react-native';
 import theme from './styles/theme';
 import { ThemeProvider } from "styled-components/native"
 import { Routes } from './routes';
+import AppLoading from 'expo-app-loading';
+
+import {
+    useFonts,
+    Poppins_300Light,
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold,
+    Poppins_800ExtraBold
+} from "@expo-google-fonts/poppins";
+
+import { DMSans_400Regular } from "@expo-google-fonts/dm-sans"
+import { DMSerifDisplay_400Regular } from '@expo-google-fonts/dm-serif-display';
 
 export default function App() {
+
+    const [fontsLoaded] = useFonts({
+        Poppins_300Light,
+        Poppins_400Regular,
+        Poppins_500Medium,
+        Poppins_700Bold,
+        Poppins_800ExtraBold,
+        DMSans_400Regular,
+        DMSerifDisplay_400Regular
+    })
+
+    if (!fontsLoaded) {
+        return <AppLoading />
+    }
 
   return (
     <ThemeProvider theme={theme}>
--- src/screens/Tela6/index.tsx
+++ src/screens/Tela6/index.tsx
@@ -3,9 +3,10 @@ import { StyleSheet, Text, View, Alert } from 'react-native';
 import { useState } from 'react';
 import { Button } from './../../components/Button/index';
 import { useNavigation } from '@react-navigation/native';
-import { Input } from '../../components/Input';
-
+import { useTheme } from "styled-components";
 import { SelectList } from 'react-native-dropdown-select-list'
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { RFValue } from "react-native-responsive-fontsize";
 
 export function Tela6() {
     const navigation = useNavigation();
@@ -14,40 +15,51 @@ export function Tela6() {
         navigation.navigate("home");
     }
 
-    const [nome, setNome] = useState('');
+    const { FONTS, COLORS } = useTheme();
 
     const [selected, setSelected] = useState('');
+    const [itemSelecionadoDescricao, setItemSelecionadoDescricao] = useState('');
 
     const data = [
-        {key:'1', value:'Mobiles', disabled:true},
+        {key:'1', value:'Mobiles'},
         {key:'2', value:'Appliances'},
         {key:'3', value:'Cameras'},
-        {key:'4', value:'Computers', disabled:true},
+        {key:'4', value:'Computers'},
         {key:'5', value:'Vegetables'},
         {key:'6', value:'Diary Products'},
-        {key:'7', value:'Drinks'},
+        {key:'7', value:'Drinks', disabled:true},
     ]
 
-    function msg() {
-        if ( !nome )
-        {
-            Alert.alert('Campos obrigatórios!', 'Preencha todos os campos!');
-            return false;
-        }
-        const mensagem: string = `Hello ${nome}`;
-        Alert.alert('Olá', mensagem);
+    const guardarSelected = ( key: string ) => {
+        setSelected(key);
+        data.forEach(element => {
+            if ( element.key == key )
+            {
+                setItemSelecionadoDescricao(element.value);
+            }
+        });
     }
 
     return (
         <Container>
             <Text>Tela 6</Text>
-            <Input onChangeText={setNome} keyboardType="default" placeholder='Nome'/>
 
             <SelectList
-            setSelected={(val) => setSelected(val)}
-            data={data}
-            save="value"
+                arrowicon={<Ionicons name="arrow-down" size={RFValue(20)} color={COLORS.GRAY2} />}
+                boxStyles={{borderRadius:0}} //override default styles
+                fontFamily={FONTS.POPPINSREGULAR}
+
+                search={true}
+                searchicon={<Ionicons name="search" size={RFValue(20)} color={COLORS.GRAY2} />}
+
+              //  onSelect={() => alert(selected)}
+                setSelected={(key) => guardarSelected(key) }
+                data={data}
+                save="key"
+                defaultOption={{ key:'1', value:'Mobiles' }}   //default selected option
             />
+
+            <Text>Opção Selecionada: {'\n'}key: {selected} {'\n'}value: {itemSelecionadoDescricao}</Text>
 
             <Button title='Home' onPress={rotaHome}/>
         </Container>
